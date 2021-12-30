db282fce8f19108645d8abfa875e33c0d1446a30
Patrick Joyce <pjaudiomv@gmail.com>
2021-12-30 14:01:51 -0500

    Deploy website - based on a1be1e3a6d8d920ae32f3a007d8439f32607c1be

---
+++ assets/js/a53cb628.214f2e32.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkyolo=self.webpackChunkyolo||[]).push([[357],{2776:function(s){s.exports=JSON.parse('{"allTagsPath":"/blog/tags","slug":"/blog/tags/formats","name":"formats","count":1,"permalink":"/blog/tags/formats"}')}}]);
